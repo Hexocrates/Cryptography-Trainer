2773cd8c054c0566cf4e716a3bc609cea630438c
Hexachords <76600657+Hexocrates@users.noreply.github.com>
2022-09-26 15:15:07 -0500

    Updated cryptotrain.py
    testing out slow_text function

--- trainer/cryptotrain.py
+++ trainer/cryptotrain.py
@@ -1,1 +1,16 @@
+import time
+
+
 print("hello World")
+
+
+def slow_text(text):
+    for char in text:
+        print (char, end='')
+        time.sleep(.08)
+
+
+
+slow_text("Hello Human,")
+slow_text("Would you like to play a game?")
+
